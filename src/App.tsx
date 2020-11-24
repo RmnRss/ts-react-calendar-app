0f113162734188f6a4273ca63bee7239c1049a76
Romain <romain.rousseau5@gmail.com>
2020-11-24 20:13:56 +0100

    adding global app div test

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 import { Calendar } from "./components/Calendar";
 import { Header } from "./components/Header";
 
-const Container = styled.header`
+const Container = styled.div`
   min-height: 100vh;
 `;
 
 function App() {
   return (
-    <Container>
+    <Container data-testid={"app-container"}>
       <Header />
       <Calendar />
     </Container>
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,10 @@
 import { render } from "@testing-library/react";
-import { format } from "date-fns";
 import React from "react";
 import App from "../App";
 
-test("renders the correct Date", () => {
-  const { getByText } = render(<App />);
-  getByText(format(new Date(), "MMMM yyyy"));
+describe("App", function () {
+  it("renders 2 divs", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("app-container").children.length).toBe(2);
+  });
 });
